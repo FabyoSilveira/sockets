90c53aefe990d2703cbaf6f9cac7c64e9e980e9a
fabyosilveira <fabyo.amorim@maxmilhas.com.br>
2023-05-15 17:58:47 -0300

    Sockets cliente e servidor funcionando para ipv6 e ipv4

--- client.c
+++ client.c
@@ -10,6 +10,69 @@
 
 char validFiles[6][6] = {".txt", ".c", ".cpp", ".py", ".tex", ".java"};
 
+int createAndConnectSockToServerIPV4orIPV6(char* ip, int port){
+  int sock;
+  struct sockaddr_in servAddrV4;
+  struct sockaddr_in6 servAddrV6;
+
+  //IPV4
+  if(inet_pton(AF_INET, ip, &servAddrV4.sin_addr) == 1){
+    printf("Versão IP: IPV4\n");
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      printf("Criação do socket falhou!\n");
+      return -1;
+    }
+    
+    memset(&servAddrV4, '0', sizeof(servAddrV4));
+    servAddrV4.sin_family = AF_INET;
+    servAddrV4.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip, &servAddrV4.sin_addr) <= 0) {
+      printf("Endereço v4 não suportado\n");
+      return -1;
+    }
+
+    printf("Endereço válido mapeado para servAddr4\n");
+
+    if (connect(sock, (struct sockaddr *)&servAddrV4, sizeof(servAddrV4)) < 0) {
+      printf("Connection Failed\n");
+      return -1;
+    }
+
+    printf("Conectado com sucesso ao server %s porta %d\n", ip, port);
+
+  //IPV6
+  }else if(inet_pton(AF_INET6, ip, &servAddrV6.sin6_addr) == 1){
+    printf("Versão IP: IPV6\n");
+
+    if ((sock = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
+      printf("Criação do socket falhou!\n");
+      return -1;
+    }
+    
+    memset(&servAddrV6, '0', sizeof(servAddrV6));
+    servAddrV6.sin6_family = AF_INET6;
+    servAddrV6.sin6_port = htons(port);
+
+    if (inet_pton(AF_INET6, ip, &servAddrV6.sin6_addr) <= 0) {
+      printf("Endereço v6 não suportado\n");
+      return -1;
+    }
+
+    printf("Endereço válido mapeado para servAddr6\n");
+
+    if (connect(sock, (struct sockaddr *)&servAddrV6, sizeof(servAddrV6)) < 0) {
+      printf("Connection Failed\n");
+      return -1;
+    }
+
+    printf("Conectado com sucesso ao server %s porta %d\n", ip, port);
+  }
+
+  return sock;
+}
+
 int main(int argc, char *argv[]){
   char buffer[BUFFER_SIZE];
   char *ip = argv[1];
@@ -23,31 +86,13 @@ int main(int argc, char *argv[]){
   printf("Endereço IP: %s\n", argv[1]);
   printf("Porta de conexão: %s\n", argv[2]);
 
-  int sock;
-  struct sockaddr_in servAddr;
+  int sock; 
+  struct sockaddr_in servAddrV4;
 
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("Criação do socket falhou!\n");
+  //Create and connect sock to server
+  if((sock = createAndConnectSockToServerIPV4orIPV6(ip, port)) < 0){
     return 0;
   }
-  
-  memset(&servAddr, '0', sizeof(servAddr));
-  servAddr.sin_family = AF_INET;
-  servAddr.sin_port = htons(port);
-
-  if (inet_pton(AF_INET, ip, &servAddr.sin_addr) <= 0) {
-    printf("Endereço não suportado\n");
-    return 0;
-  }
-
-  printf("Endereço válido mapeado para servAddr\n");
-
-  if (connect(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
-    printf("Connection Failed\n");
-    return 0;
-  }
-
-  printf("Conectado com sucesso ao server %s porta %d\n", ip, port);
 
   while(1){
     fgets(buffer, sizeof(buffer), stdin);
@@ -97,20 +142,20 @@ int main(int argc, char *argv[]){
       printf("Send message file: %s\n", fileName);
       //send file  
       if (send(sock, fileName, sizeof(fileName), 0) == -1){
-        printf("Send message failed");
+        printf("Send message failed\n");
         return 0;
       }   
 
-      printf("Send succesfully!");
+      printf("Send succesfully!\n");
     }else if(strcmp(buffer, "exit") == 0){
            
       if (send(sock, buffer, sizeof(buffer), 0) == -1){
-        printf("Send message failed");
+        printf("Send message failed\n");
         return 0;
       } 
 
       if(close(sock) == -1){
-        printf("Erro ao fechar o socket");
+        printf("Erro ao fechar o socket\n");
       }
 
       break;
--- server.c
+++ server.c
@@ -8,24 +8,14 @@
 
 #define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[]){
-  char buffer[BUFFER_SIZE]; 
-  
-  char *ipVersion = argv[1];
-  int port = atoi(argv[2]);
-
-  printf("Server running!\n");
-  printf("Versão IP: %s\n", argv[1]);
-  printf("Porta: %s\n", argv[2]);
-
-  int sockfd, newSock;
-  struct sockaddr_in servAddr, cliAddr;
-  socklen_t cliLen = sizeof(cliAddr);
+int createAndConfigureSocketIPV4(int port){
+  int sockfd;
+  struct sockaddr_in servAddr;
 
   // Cria o socket
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("Criação do socket falhou!\n");
-    return 0;
+    return -1;
   }
 
   printf("Socket criado com sucesso!\n");
@@ -39,7 +29,7 @@ int main(int argc, char *argv[]){
   // Associa o socket a um endereço e porta
   if (bind(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
     printf("Bind do socket à porta e endereço falhou\n");
-    return 0;
+    return -1;
   }
 
   printf("Socket associado a porta e endereço com sucesso!\n");
@@ -47,24 +37,124 @@ int main(int argc, char *argv[]){
   // Aguarda a conexão de um cliente
   if (listen(sockfd, 1) < 0) {
     printf("Listen do socket falhou!\n");
-    return 0;
+    return -1;
   }
 
   printf("Socket server ouvindo a porta: %d\n", port);
 
+  return sockfd;
+}
+
+int receiveClientConnectionIPV4(int serverSock){
+  int newSock;
+  struct sockaddr_in cliAddr;
+  socklen_t cliLen = sizeof(cliAddr);
+
   // Aceita a conexão do cliente
-  if ((newSock = accept(sockfd, (struct sockaddr *) &cliAddr, &cliLen)) < 0) {
+  if ((newSock = accept(serverSock, (struct sockaddr *) &cliAddr, &cliLen)) < 0) {
     printf("Accept conexão do cliente falhou!\n");
-    return 0;
+    return -1;
   }
 
   printf("Cliente conectado!\n");
+
+  return newSock;
+}
+
+int createAndConfigureSocketIPV6(int port){
+  int sockfd;
+  struct sockaddr_in6 servAddr;
+
+  // Cria o socket
+  if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
+    printf("Criação do socket falhou!\n");
+    return -1;
+  }
+
+  printf("Socket criado com sucesso!\n");
+
+  // Configura a estrutura do endereço do servidor
+  memset(&servAddr, 0, sizeof(servAddr));
+  servAddr.sin6_family = AF_INET6;
+  servAddr.sin6_addr = in6addr_any;
+  servAddr.sin6_port = htons(port);
+
+  // Associa o socket a um endereço e porta
+  if (bind(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+    printf("Bind do socket à porta e endereço falhou\n");
+    return -1;
+  }
+
+  printf("Socket associado a porta e endereço com sucesso!\n");
+
+  // Aguarda a conexão de um cliente
+  if (listen(sockfd, 1) < 0) {
+    printf("Listen do socket falhou!\n");
+    return -1;
+  }
+
+  printf("Socket server ouvindo a porta: %d\n", port);
+
+  return sockfd;
+}
+
+int receiveClientConnectionIPV6(int serverSock){
+  int newSock;
+  struct sockaddr_in6 cliAddr;
+  socklen_t cliLen = sizeof(cliAddr);
+
+  // Aceita a conexão do cliente
+  if ((newSock = accept(serverSock, (struct sockaddr *) &cliAddr, &cliLen)) < 0) {
+    printf("Accept conexão do cliente falhou!\n");
+    return -1;
+  }
+
+  printf("Cliente conectado!\n");
+
+  return newSock;
+}
+
+int main(int argc, char *argv[]){
+  char buffer[BUFFER_SIZE]; 
+  
+  char *ipVersion = argv[1];
+  int port = atoi(argv[2]);
+
+  printf("Server running!\n");
+  printf("Versão IP: %s\n", argv[1]);
+  printf("Porta: %s\n", argv[2]);
+
+  int sockfd, cliSock;
+  
+  if(strcmp(ipVersion, "v4") == 0){
+    printf("Configurando conexão IPV4!\n");
+
+    //Create sock and accept client connection
+    if((sockfd = createAndConfigureSocketIPV4(port)) < 0){
+      return 0;
+    }
+    if((cliSock = receiveClientConnectionIPV4(sockfd)) < 0){
+      return 0;
+    }
+  }else if(strcmp(ipVersion, "v6") == 0){
+    printf("Configurando conexão IPV6!\n");
+
+    //Create sock and accept client connection
+    if((sockfd = createAndConfigureSocketIPV6(port)) < 0){
+      return 0;
+    }
+    if((cliSock = receiveClientConnectionIPV6(sockfd)) < 0){
+      return 0;
+    }
+  }else{
+    printf("Versão ip recebida inválida!\n");
+  }
 
   int recvReturn;
 
   while(1){
     memset(buffer, 0, sizeof(buffer));
-    recvReturn = recv(newSock, buffer, sizeof(buffer), 0);
+    recvReturn = recv(cliSock, buffer, sizeof(buffer), 0);
 
     if(recvReturn < 0){
       printf("Erro ao executar recv do client\n");
@@ -77,7 +167,7 @@ int main(int argc, char *argv[]){
         printf("Erro ao fechar o socket do servidor!\n");
       }
 
-      if(close(newSock) == -1){
+      if(close(cliSock) == -1){
         printf("Erro ao fechar o socket do cliente!\n");
       }
 
